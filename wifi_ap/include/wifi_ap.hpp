783e4487ff31ecc97b0b1a7ed2a854e79000e0f7
Anne Brondijk <73667900+abrondijk@users.noreply.github.com>
2022-10-12 21:34:17 +0200

    feat: Components setup

---
+++ wifi_ap/include/wifi_ap.hpp
@@ -0,0 +1,13 @@
+#ifndef WIFI_AP_HPP
+#define WIFI_AP_HPP
+
+class WifiAP {
+
+public:
+    WifiAP ();
+private:
+    static const inline char TAG[] = "Wifi AP";
+
+};
+
+#endif /* WIFI_AP_HPP */
---
+++ wifi_sta/include/wifi_sta.hpp
@@ -0,0 +1,13 @@
+#ifndef WIFI_STA_HPP
+#define WIFI_STA_HPP
+
+class WifiSta {
+
+public:
+    WifiSta ();
+private:
+    static const inline char TAG[] = "Wifi STA";
+
+};
+
+#endif /* WIFI_STA_HPP */
