1034cd6456f1bef584e4767ffa7c7b986d635b55
Anne Brondijk <73667900+abrondijk@users.noreply.github.com>
2024-05-09 22:11:10 +0200

    feat: esp_err_t std::error_category implementation

---
+++ util/include/esp_system_error.hpp
@@ -0,0 +1,34 @@
+#ifndef ESP_SYSTEM_ERROR_HPP
+#define ESP_SYSTEM_ERROR_HPP
+
+#include <system_error>
+
+#include "esp_err.h"
+
+namespace sdk {
+
+    class EspErrorCategory : public std::error_category {
+    public:
+        const char* name() const noexcept override {
+            return "esp_err_t";
+        }
+
+        std::string message(int ev) const override {
+            return {esp_err_to_name(static_cast<esp_err_t>(ev))};
+        }
+    };
+
+} // namespace sdk
+
+
+namespace std {
+    template<>
+    struct is_error_code_enum<sdk::EspErrorCategory> : true_type {};
+
+    std::error_code make_error_code(esp_err_t e) {
+        static sdk::EspErrorCategory category;
+        return {static_cast<int>(e), category};
+    }
+} // namespace std
+
+#endif // ESP_SYSTEM_ERROR_HPP
